f1111599c9e781c37f585507a71290ce614c060b
Mahalakshmi Sivakumar <smahalakshmikumar@gmail.com>
2025-08-16 15:55:56 +0200

    feat:remove redundant

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,6 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   compress: true,
-  swcMinify: true,
-  legacyBrowsers: false,
-  browsersListForSwc: true,
 };
 
 export default nextConfig;
